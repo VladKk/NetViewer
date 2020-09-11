9913e258004305a8d20ae827138707145dd8ddd1
VladKk <mrvlad008@gmail.com>
2020-09-11 13:56:48 +0300

    Add main network information

--- src/main_win/main_win.cpp
+++ src/main_win/main_win.cpp
@@ -1,5 +1,7 @@
 #include <QDebug>
 #include <QMessageBox>
+#include <QHostAddress>
+#include <QNetworkInterface>
 
 #include "main_win.h"
 #include "./ui_main_win.h"
@@ -12,6 +14,27 @@ MainWin::MainWin(QWidget *parent)
     connect(ui->actionAbout_developer, SIGNAL(triggered()), this, SLOT(actions()));
     connect(ui->actionAbout_program, SIGNAL(triggered()), this, SLOT(actions()));
     connect(ui->actionExit, SIGNAL(triggered()), this, SLOT(actions()));
+
+    const QHostAddress &localhost = QHostAddress(QHostAddress::LocalHost);
+    for (const QNetworkInterface &eth : QNetworkInterface::allInterfaces()) {
+        for (const QNetworkAddressEntry &entry : eth.addressEntries()) {
+            if (entry.ip().protocol() == QAbstractSocket::IPv4Protocol && entry.ip() != localhost) {
+                ui->labelIpv4->setText("IPv4: " + entry.ip().toString());
+                ui->labelMask->setText("Netmask: " + entry.netmask().toString());
+                ui->labelBroadcast->setText("Broadcast: " + entry.broadcast().toString());
+            } else if (entry.ip().protocol() == QAbstractSocket::IPv6Protocol && entry.ip() != localhost) {
+                ui->labelIpv6->setText("IPv6: " + entry.ip().toString());
+                ui->labelPrefixlen->setText("Prefix length: " + QString::number(entry.prefixLength()));
+            } else {
+                ui->labelIpv4->setText("IPv4: " + localhost.toString());
+                ui->labelMask->setText("Netmask: " + entry.netmask().toString());
+                ui->labelBroadcast->setText("Broadcast: " + entry.broadcast().toString());
+            }
+        }
+
+        if (!(eth.flags() & QNetworkInterface::IsLoopBack))
+            ui->labelMac->setText("MAC: " + eth.hardwareAddress());
+    }
 }
 
 MainWin::~MainWin() { delete ui; }
