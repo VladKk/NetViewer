c96bcd7d4c36747e5c85878032ff94d406f03d76
VladKk <mrvlad008@gmail.com>
2020-09-13 10:40:48 +0300

    Comment the code + Add status bar

--- src/main_win/main_win.cpp
+++ src/main_win/main_win.cpp
@@ -17,6 +17,7 @@ MainWin::MainWin(QWidget *parent)
     setWindowIcon(QIcon(":/icons/mainWinIcon.ico"));
     ui->setupUi(this);
 
+    // Connect elements with appropriate slots
     connect(ui->actionAbout_developer, SIGNAL(triggered()), this, SLOT(actions()));
     connect(ui->actionAbout_program, SIGNAL(triggered()), this, SLOT(actions()));
     connect(ui->actionExit, SIGNAL(triggered()), this, SLOT(actions()));
@@ -26,23 +27,27 @@ MainWin::MainWin(QWidget *parent)
 
     connect(ui->buttonCheckConnection, SIGNAL(pressed()), this, SLOT(checkConnection()));
 
+    // Check all address entries in network interface and print main information
     const QHostAddress &localhost = QHostAddress(QHostAddress::LocalHost);
     for (const QNetworkInterface &eth : QNetworkInterface::allInterfaces()) {
         for (const QNetworkAddressEntry &entry : eth.addressEntries()) {
+            // For IPv4, netmask and broadcast
             if (entry.ip().protocol() == QAbstractSocket::IPv4Protocol && entry.ip() != localhost) {
                 ui->labelIpv4->setText("IPv4: " + entry.ip().toString());
                 ui->labelMask->setText("Netmask: " + entry.netmask().toString());
                 ui->labelBroadcast->setText("Broadcast: " + entry.broadcast().toString());
+                // For IPv6 and prefix length
             } else if (entry.ip().protocol() == QAbstractSocket::IPv6Protocol && entry.ip() != localhost) {
                 ui->labelIpv6->setText("IPv6: " + entry.ip().toString());
                 ui->labelPrefixlen->setText("Prefix length: " + QString::number(entry.prefixLength()));
+                // For localhost
             } else {
                 ui->labelIpv4->setText("IPv4: " + localhost.toString());
                 ui->labelMask->setText("Netmask: " + entry.netmask().toString());
                 ui->labelBroadcast->setText("Broadcast: " + entry.broadcast().toString());
             }
         }
-
+        // For MAC address
         if (!(eth.flags() & QNetworkInterface::IsLoopBack))
             ui->labelMac->setText("MAC: " + eth.hardwareAddress());
     }
@@ -51,8 +56,10 @@ MainWin::MainWin(QWidget *parent)
 MainWin::~MainWin() { delete ui; }
 
 void MainWin::actions() {
+    // Get signal from any QAction
     auto *action = (QAction *) sender();
 
+    // Trigger action if it is valid, otherwise exit with error
     if (action == ui->actionAbout_developer) {
         QMessageBox::information(this, "NetViewer",
                                  "Find me in the Web!\n"
@@ -76,9 +83,9 @@ void MainWin::actions() {
 }
 
 void MainWin::checkConnection() {
-    struct ifaddrs *ifaddr, *ifa;
-    int family, n;
-    qint32 download, upload;
+    struct ifaddrs *ifaddr, *ifa; // Structs for interfaces
+    int family, n; //
+    qint32 download, upload; // Download and upload speed
     QString interface_name;
 
     if (getifaddrs(&ifaddr) == -1) {
@@ -93,23 +100,27 @@ void MainWin::checkConnection() {
         interface_name = "lo";
 
     for (ifa = ifaddr, n = 0; ifa != nullptr; ifa = ifa->ifa_next, n++) {
-        if (ifa->ifa_addr == nullptr)
+        if (ifa->ifa_addr == nullptr) // If address is empty, continue
             continue;
 
-        family = ifa->ifa_addr->sa_family;
+        family = ifa->ifa_addr->sa_family; // Get interface family
 
+        // If family is AF_PACKET and interface name is correct, get link statistics
         if ((family == AF_PACKET) && (ifa->ifa_data != nullptr) && (ifa->ifa_name == interface_name)) {
             auto *stats = static_cast<struct rtnl_link_stats *>(ifa->ifa_data);
+            // Get downloaded (rx) and uploaded (tx) bytes
             download = stats->rx_bytes;
             upload = stats->tx_bytes;
         }
     }
 
-    freeifaddrs(ifaddr);
+    freeifaddrs(ifaddr); // Free pointer to interface address
 
+    // Print connection speed in Bytes/second
     ui->labelDownload->setText("Download Speed: " + QString::number(download) + " B/s");
     ui->labelUpload->setText("Upload Speed: " + QString::number(upload) + " B/s");
 
+    // Transform speed in MB/s or KB/s if needed
     if (download > 1000) {
         download = download / 1024;
         if (download > 1000) {
@@ -129,5 +140,9 @@ void MainWin::checkConnection() {
     }
 }
 
-void MainWin::refresh() { QTimer::singleShot(1000, this, SLOT(checkConnection())); }
+void MainWin::refresh() {
+    // Recheck speed in 1 second
+    ui->statusBar->showMessage("Checking connection...", 1000);
+    QTimer::singleShot(1000, this, SLOT(checkConnection()));
+}
 
